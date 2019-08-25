7796c555f80e223edeff1a59e4d61d524ded609c
Rafael Saraceni <saraceni.br@gmail.com>
2019-08-25 12:19:27 -0300

    First commit

---
+++ precache-manifest.df03d43c8bd0b5a711ca71dfd6e44b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b601f9001833464bfe74562f3bf48e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96ca7a57d713a913fbc9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d5b643da7f88580c33c0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "96ca7a57d713a913fbc9",
+    "url": "/static/js/2.2980e131.chunk.js"
+  },
+  {
+    "revision": "d5b643da7f88580c33c0",
+    "url": "/static/js/main.ea3f550f.chunk.js"
+  },
+  {
+    "revision": "f13fcee40ea4fad0df38",
+    "url": "/static/js/runtime~main.61c1c940.js"
+  },
+  {
+    "revision": "a19e59f0660b7eb669fcf9766b30dbe0",
+    "url": "/static/media/Icon_Rounded.a19e59f0.png"
+  }
+]);
